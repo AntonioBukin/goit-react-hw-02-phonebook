b930e228ff96cbf126580435f37c340d747a5501
AntonioBukin <bav1905@gmail.com>
2023-07-05 15:54:36 +0300

    add some info

--- src/modules/MyPhone/MyPhone.jsx
+++ src/modules/MyPhone/MyPhone.jsx
@@ -11,17 +11,31 @@ class MyPhone extends Component {
             <div className={styles.wrapper}>
                 <h3 className={styles.title}>My Phone</h3>
                 <div className={styles.blocks}>
-                    <MyPhoneBlock title="Add number">
-                        <form>
-                            <div>
-                                <label></label>
-                                <input/>
+                    <MyPhoneBlock title="">
+                        <form className={styles.form}>
+                            <div className={styles.formGroup}>
+                                <label>Name</label>
+                                <input className={styles.textField} placeholder="add name"
+                                  type="text"
+                                  name="name"
+                                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                                  required/>
+                                {/* <input className={styles.textField} placeholder="add name"/> */}
                             </div>
+                            <button type="submit">Add contacts</button>
                         </form>
-
                     </MyPhoneBlock>
-                    <MyPhoneBlock title="Phone list">
-
+                    <MyPhoneBlock title="Find contacts by name">
+                        <input className={styles.textField} placeholder="enter number" />
+                        <ol className={styles.list}>
+                            <li className={styles.listItem}>
+                                Rose Simpson: 459-12-56 <button>delete</button>
+                            </li>
+                            <li className={styles.listItem}>
+                                Hermione Cline: 458-13-81 <button>delete</button>
+                            </li>
+                        </ol>
                     </MyPhoneBlock>
 
                 </div>
