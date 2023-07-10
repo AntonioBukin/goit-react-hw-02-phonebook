d1b8cd1f1084667e7a5fa8a70b0c95c144b5f1cc
AntonioBukin <bav1905@gmail.com>
2023-07-10 16:09:05 +0300

    add parts of form

--- src/modules/MyPhone/MyPhone.jsx
+++ src/modules/MyPhone/MyPhone.jsx
@@ -2,6 +2,8 @@ import { Component } from "react";
 import { nanoid } from "nanoid";
 
 import MyPhoneBlock from "./MyPhoneBlock/MyPhoneBlock";
+import MyPhoneList from "./MyPhoneList/MyPhoneList";
+import MyPhoneForm from "./MyPhoneForm/MyPhoneForm";
 
 import styles from "./my-phone.module.scss";
 
@@ -9,43 +11,30 @@ import styles from "./my-phone.module.scss";
 class MyPhone extends Component {
     state = {
         contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+            // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
           ],
-          filter: "",
-          name: "",
-          number: "",
+          filter: "",          
     }
 
-    handleChange = ({target}) => {
-        const {name, value} = target; //value - значення яке ми записуємо в state
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        if(this.isDublicate()) {
-            const {name, number} = this.state;
-            alert(`${name} - ${number} is alredy in contacts`);
-            return;
+    onAddPhone = ({name, number}) => {
+        if(this.isDublicate({name, number})) {
+            return alert(`${name} - ${number} is alredy in contacts`);
         }
         this.setState(prevState => {
-            const {name, number, contacts} = prevState;
-            const newNumber = {
+            const {contacts} = prevState;
+            const newPhone = {
                 id: nanoid(),
                 name,
                 number,
             }
-
-            return {contacts: [...contacts, newNumber], name: "", number: ""}
+            return {contacts: [...contacts, newPhone]}
         })
     }
 
-    onDeletePhone(id) {
+    onDeletePhone = (id) => {
         this.setState(prevState => {
             const newContacts = prevState.contacts.filter(contact => contact.id !== id)
 
@@ -55,8 +44,8 @@ class MyPhone extends Component {
         })
     }
 
-    isDublicate() {
-        const {name, number, contacts} = this.state;
+    isDublicate({name, number}) {
+        const {contacts} = this.state;
         const normalizedName = name.toLowerCase();
         const normalizedNumber = number.toLowerCase();
 
@@ -81,60 +70,23 @@ class MyPhone extends Component {
     }
 
     render() { //пишемо метод render, який буде повертати розмітку
-        const {name, number} = this.state;
-
         const contacts = this.getFilteredPhone();
-
-        const elements = contacts.map(({id, name, number}) => (
-            <li className={styles.listItem} key={id}>
-                Name: {name}, Number: {number}. <button onClick={()=> this.onDeletePhone(id)}>delete</button>
-            </li>
-
-        ))
 
         return(
             <div className={styles.wrapper}>
                 <h3 className={styles.title}>My Phone</h3>
                 <div className={styles.blocks}>
                     <MyPhoneBlock title="">
-                        <form onSubmit={this.handleSubmit} className={styles.form}>
-                            <div className={styles.formGroup}>
-                                <label>Name</label>
-                                <input value={name} name="name" onChange={this.handleChange} className={styles.textField} placeholder="add name"
-                                  type="text"
-                                //   name="name"
-                                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                                  required/>
-                                {/* <input className={styles.textField} placeholder="add name"/> */}
-                            </div>
-                            <div className={styles.formGroup}>
-                                <label>Number</label>
-                                <input value={number} name="number" onChange={this.handleChange} className={styles.textField} placeholder="add number"
-                                  type="text"
-                                //   name="name"
-                                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                                  required/>
-                                {/* <input className={styles.textField} placeholder="add name"/> */}
-                            </div>
-                            <button type="submit">Add contacts</button>
-                        </form>
+                        <MyPhoneForm onSubmit={this.onAddPhone}/>                       
                     </MyPhoneBlock>
                     <MyPhoneBlock title="Find contacts by name">
                         <input name="filter" onChange={this.handleChange} className={styles.textField} placeholder="enter number" />
-                        <ol className={styles.list}>
-                            {elements}
-                        </ol>
+                       <MyPhoneList contacts={contacts} onDeletePhone={this.onDeletePhone}/>
                     </MyPhoneBlock>
-
                 </div>
             </div>
         )
-
     }
-
 }
-
 
 export default MyPhone;
---
+++ src/modules/MyPhone/MyPhoneList/MyPhoneList.jsx
@@ -0,0 +1,17 @@
+import styles from "./my-phone-list.module.scss";
+
+const MyPhoneList = ({contacts, onDeletePhone}) => {
+    const elements = contacts.map(({id, name, number}) => (
+        <li className={styles.listItem} key={id}>
+            Name: {name}, Number: {number}. <button onClick={()=> onDeletePhone(id)}>delete</button>
+        </li>
+    ))
+
+    return ( 
+    <ol className={styles.list}>
+        {elements}
+    </ol>)
+
+}
+
+export default MyPhoneList;
